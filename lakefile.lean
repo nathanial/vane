575d09a7aef941b9547a2932d187e8ccf18dc048
Nathanial Hartman <nathanial.hartman@gmail.com>
2025-12-31 01:33:49 -0800

    Update afferent dependency to v0.0.2

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ open System (FilePath)
 package vane where
   version := v!"0.1.0"
 
-require afferent from git "https://github.com/nathanial/afferent" @ "v0.0.1"
+require afferent from git "https://github.com/nathanial/afferent" @ "v0.0.2"
 require crucible from git "https://github.com/nathanial/crucible" @ "v0.0.1"
 
 -- Common link arguments (inherit from afferent for Metal/Cocoa)
